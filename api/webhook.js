db0dca57e616b018260c0d828a353717b261fcb7
Akshay10258 <akshayachar1913518@gmail.com>
2025-03-06 09:45:53 +0530

    error fixing

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -29,7 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/type-font', express.static(__dirname + '/fonts'));
+app.use('/type-font', express.static(path.join(__dirname, '../fonts')));
 
 app.post("/api/webhook", async (req, res) => {
     const userQuery = req.body.queryResult?.queryText.toLowerCase() || "";
